af827cbfa69450bce09c82a5892fb077146c134c
Luis De La Espriella <luisdelae@gmail.com>
2016-05-23 13:21:09 -0500

    added employee form

---
+++ PrimeChallenge1/Models/Employees.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrimeChallenge1.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name="Emp ID")]
+        public int IdNum { get; set; }
+        public string Title { get; set; }
+        public double Salary { get; set; }
+    }
+}
